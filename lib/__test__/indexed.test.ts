374e1924c91961ba1923dca03f3e6971d77e3803
lss <lss.rp@tom.com>
2024-10-18 17:33:27 +0800

    feat: 整理基础测试用例

--- lib/__test__/indexed.test.ts
+++ lib/__test__/indexed.test.ts
@@ -3,23 +3,58 @@ import { useIndexedDB, getDatabases, existsDatabase, deleteDatabase } from "../i
 import { getVersion } from "../database"
 
 useIndexedDB(indexedDB)
+const databaseName = "local-indexed"
+const last = 10
 
-describe("fake-indexed", () => {
+describe("create database", () => {
     it("no database at the beginning", async () => {
         expect((await getDatabases()).length).toBe(0)
-        expect(await existsDatabase("local-indexed")).toBe(false)
+        expect(await existsDatabase(databaseName)).toBe(false)
     })
     it("create a test database", async () => {
-        expect(await getVersion("local-indexed")).toBe(1)
+        expect(await getVersion(databaseName)).toBe(1)
         const databases = await getDatabases()
         expect(databases.length).toBe(1)
-        expect(databases[0].name).toBe("local-indexed")
+        expect(databases[0].name).toBe(databaseName)
         expect(databases[0].version).toBe(1)
-        expect(await existsDatabase("local-indexed")).toBe(true)
+        expect(await existsDatabase(databaseName)).toBe(true)
     })
     it("delete the test database", async () => {
-        await deleteDatabase("local-indexed")
+        await deleteDatabase(databaseName)
         expect((await getDatabases()).length).toBe(0)
-        expect(await existsDatabase("local-indexed")).toBe(false)
+        expect(await existsDatabase(databaseName)).toBe(false)
+    })
+})
+
+describe("create databases", () => {
+    it("no database at the beginning", async () => {
+        expect((await getDatabases()).length).toBe(0)
+        expect(await existsDatabase(databaseName)).toBe(false)
+    })
+    it("create test databases", async () => {
+        for (let i = 1; i <= last; i++) {
+            expect(await getVersion(`${databaseName}-${i}`)).toBe(1)
+        }
+        const databases = await getDatabases()
+        expect(databases.length).toBe(last)
+        for (let i = 1; i <= last; i++) {
+            const { name, version } = databases[i - 1]
+            if (!name) throw new Error("name should not be undefined")
+            expect(await existsDatabase(name)).toBe(true)
+            expect(name).toBe(`${databaseName}-${i}`)
+            expect(version).toBe(1)
+        }
+    })
+    it("delete the test databases", async () => {
+        await deleteDatabase(databaseName)
+        const databases = await getDatabases()
+        expect(databases.length).toBe(last)
+        for (let i = 1; i <= last; i++) {
+            const { name } = databases[i - 1]
+            if (!name) throw new Error("name should not be undefined")
+            await deleteDatabase(name)
+            expect(await existsDatabase(name)).toBe(false)
+        }
+        expect((await getDatabases()).length).toBe(0)
     })
 })
--- lib/__test__/upgrade.test.ts
+++ lib/__test__/upgrade.test.ts
@@ -1,61 +1,60 @@
 import "fake-indexeddb/auto"
 import { getDatabases, existsDatabase, deleteDatabase } from "../indexed"
 import { getVersion } from "../database"
-import { upgradeDatabase, type IDBStoreOption } from "../upgrade"
+import { upgradeDatabase } from "../upgrade"
 
-const storeOption: IDBStoreOption = {
+const databaseName = "local-indexed"
+const storeName = "test-store"
+const storeOption: IDBObjectStoreParameters = {
     keyPath: "id",
-    autoIncrement: true,
-    index: {
-        name: { unique: false }
-    }
+    autoIncrement: true
 }
 
 describe("upgrade database", () => {
     it("no database at the beginning", async () => {
         expect((await getDatabases()).length).toBe(0)
-        expect(await existsDatabase("local-indexed")).toBe(false)
+        expect(await existsDatabase(databaseName)).toBe(false)
     })
     it("create and upgrade a database", async () => {
-        await upgradeDatabase("local-indexed", 1, (context) => {
-            context.createStore("testStore", storeOption)
+        await upgradeDatabase(databaseName, 1, (database) => {
+            database.createObjectStore(storeName, storeOption)
         })
-        expect(await getVersion("local-indexed")).toBe(1)
+        expect(await getVersion(databaseName)).toBe(1)
         const databases = await getDatabases()
         expect(databases.length).toBe(1)
-        expect(databases[0].name).toBe("local-indexed")
+        expect(databases[0].name).toBe(databaseName)
         expect(databases[0].version).toBe(1)
-        expect(await existsDatabase("local-indexed")).toBe(true)
+        expect(await existsDatabase(databaseName)).toBe(true)
     })
     it("delete the test database", async () => {
-        await deleteDatabase("local-indexed")
+        await deleteDatabase(databaseName)
         expect((await getDatabases()).length).toBe(0)
-        expect(await existsDatabase("local-indexed")).toBe(false)
+        expect(await existsDatabase(databaseName)).toBe(false)
     })
 })
 
 describe("upgrade database 10 times", () => {
     it("no database at the beginning", async () => {
         expect((await getDatabases()).length).toBe(0)
-        expect(await existsDatabase("local-indexed")).toBe(false)
+        expect(await existsDatabase(databaseName)).toBe(false)
     })
     it("create and upgrade a database", async () => {
         const last = 10
         for (let i = 1; i <= last; i++) {
-            await upgradeDatabase("local-indexed", i, (context) => {
-                context.createStore(`testStore-${i}`, storeOption)
+            await upgradeDatabase(databaseName, i, (database) => {
+                database.createObjectStore(`${storeName}-${i}`, storeOption)
             })
-            expect(await getVersion("local-indexed")).toBe(i)
+            expect(await getVersion(databaseName)).toBe(i)
         }
         const databases = await getDatabases()
         expect(databases.length).toBe(1)
-        expect(databases[0].name).toBe("local-indexed")
+        expect(databases[0].name).toBe(databaseName)
         expect(databases[0].version).toBe(last)
-        expect(await existsDatabase("local-indexed")).toBe(true)
+        expect(await existsDatabase(databaseName)).toBe(true)
     })
     it("delete the test database", async () => {
-        await deleteDatabase("local-indexed")
+        await deleteDatabase(databaseName)
         expect((await getDatabases()).length).toBe(0)
-        expect(await existsDatabase("local-indexed")).toBe(false)
+        expect(await existsDatabase(databaseName)).toBe(false)
     })
 })
