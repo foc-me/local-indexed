9e162ae1c335ad0995ede4b6cf8596ddc11a6bb5
luuming <luuming@hotmail.com>
2024-12-10 18:04:19 +0800

    feat: 类型声明

---
+++ src/upgrade.ts
@@ -0,0 +1,31 @@
+import { upgradeAction } from "./lib/upgrade"
+import { type LDBContext } from "./context"
+
+/**
+ * upgrade context
+ */
+export interface LDBUpgradeEvent {
+    oldVersion: number
+    newVersion: number | null
+}
+
+/**
+ * indexed upgrade
+ * 
+ * @param version upgrade version
+ * @param callback upgrade callback
+ * @param context database context
+ */
+export async function upgrade(
+    context: LDBContext,
+    version: number,
+    callback: (event: LDBUpgradeEvent) => void | Promise<void>
+) {
+    await upgradeAction(context.database, version, (event) => {
+        const { transaction, oldVersion, newVersion } = event
+        context.setTransaction(transaction)
+        return callback({ oldVersion, newVersion })
+    }, false, context.indexedDB).finally(() => {
+        context.setTransaction()
+    })
+}
